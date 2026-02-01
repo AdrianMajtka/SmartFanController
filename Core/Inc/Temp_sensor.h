5d03ce0ec1239c635d6298146421b6e39bd364f0
AdrianMajtka <adrianmajtka5@gmail.com>
2026-02-01 23:00:40 +0100

    Przeliczanie temperatury z LM35 i poprawka DMAt
    Zastosowałem rzutowanie (uint32_t), aby funkcja DMA mogła poprawnie odczytać adres tablicy uint16_t, bez rzutowania wyskakiwały błędy.
    Zrobiłem zamianę sygnału na stopnie Celsjusza.
    Zastosowałem formatowanie w sprintf aby na wyświetlaczu mieć 2 miejsca po przecinku.

--- Core/Inc/Temp_sensor.h
+++ Core/Inc/Temp_sensor.h
@@ -7,11 +7,11 @@
 
 #ifndef INC_TEMP_SENSOR_H_
 #define INC_TEMP_SENSOR_H_
-$include "main.h"
+#include "main.h"
 
-float TEMP_Read(void);
-
-void TEMP_Format(float value, char* buffor);
+void Temp_init(ADC_HandleTypeDef* hadc);
+float Temp_Read(void);
+void Temp_Format(float suma, char* buffor);
 
 
 #endif /* INC_TEMP_SENSOR_H_ */
--- Core/Src/Temp_sensor.c
+++ Core/Src/Temp_sensor.c
@@ -1,8 +1,33 @@
-/*
- * Temp_sensor.c
- *
- *  Created on: 1 lut 2026
- *      Author: 48783
- */
+#include "Temp_Sensor.h"
+#include "main.h"
+#include <stdio.h>
+
+static uint16_t adc_bufor[100];
+
+void Temp_init(ADC_HandleTypeDef* hadc)
+{
+	HAL_ADC_Start_DMA(hadc, (uint32_t*)adc_bufor, 100);
+}
+
+float Temp_Read(void)
+{
+	uint8_t i;
+	uint32_t suma=0;
+	float temp;
+	for(i=0;i<=99; i++)
+	{
+		suma=suma+adc_bufor[i];
+	}
+	temp=(((float)suma/100)*330)/4095;
+	return temp;
+}
+
+void Temp_Format(float suma, char* bufor)
+{
+	sprintf(bufor, "%.2f", suma);
+
+}
+
+
 
 
